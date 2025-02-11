76ece8d6cd47e0a0acaf6232602932029eee1690
Mazidul <md.mazidulhasan1@gmail.com>
2025-02-11 23:32:31 +0600

    Add api token to browser

--- tests/webApi.spec.js
+++ tests/webApi.spec.js
@@ -1,50 +1,34 @@
 const { test , expect, request } = require("@playwright/test")
 
 const loginPayload = {
-    username: "Admin",
-    password: "admin123"
+    "userEmail": "test1010@gmail.com", 
+    "userPassword": "7RMW#L5XHCgwEsK"
 }
 let token;
-
-
 test.beforeAll(async ({  }) => {
     const apiContext = await request.newContext()
-    const loginResponse = await apiContext.post("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
     {
         data: loginPayload
     })
-    console.log("loginResponse is", loginResponse)
-    expect(loginResponse.status()).toBe(200)
+    expect(loginResponse.ok()).toBeTruthy()
     const responseBody = await loginResponse.json()
-    console.log(responseBody)
     token = responseBody.token
     console.log(token)
-    
-
 })
 
 
 
 
 test.describe("Web API Tests", () => {
-    // test.beforeEach(async ({ request }) => {
-    //     await request.newContext({
-    //         extraHTTPHeaders: {     
-    //             "Authorization": `Bearer ${token}`
-    //         }
-    //     })
-    // })
+    
+    test("Client App login", async ({ page }) => {
+        console.log("token is 2", token);
+        await page.addInitScript(value => {
+            window.localStorage.setItem('token', value)
+        },token);
 
-    test("Get all employees", async ({ page }) => {
-        await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        await page.getByPlaceholder("Username").fill("Admin")
-        await page.getByPlaceholder("Password").fill("admin123")
-        await page.getByRole("button", { name: "Login" }).click()
-        await page.getByRole("link", { name: "PIM" }).click()
-        await page.getByRole("link", { name: "Add Employee" }).click()
-        await page.getByPlaceholder("First Name").fill("Anshika")
-        await page.getByPlaceholder("Last Name").fill("Kumari")
-        await page.getByRole("button", { name: "Save" }).click()
+        await page.goto("https://rahulshettyacademy.com/client")
     })
 
 })
